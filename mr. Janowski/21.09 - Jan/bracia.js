7299f64f5c0cf03128d0be8dd3c9152196bf2252
Janmuszynianka <40071392+Janmuszynianka@users.noreply.github.com>
2023-09-21 13:02:02 +0200

    21.09

---
+++ mr. Janowski/21.09 - Jan/bracia.js
@@ -0,0 +1,18 @@
+function marek(){
+    return 170
+}
+
+function kamil(){
+    return marek() - 40
+}
+
+function adam(){
+    return kamil() + 60
+}
+
+function sufit(){
+    return adam() + 60
+}
+
+console.log(sufit())
+
