3f90e708c310192f8e75559d372379ada497cab2
Janmuszynianka <40071392+Janmuszynianka@users.noreply.github.com>
2023-10-18 14:54:12 +0200

    18.10
    Znajdowanie oraz strona

---
+++ mr. Sosnowski/18.10/Zadanie-funkcje/skrypt.js
@@ -0,0 +1,7 @@
+
+function fontColor() {
+    let wybor = document.getElementById("fontcolor");
+    let wybrany = wybor.value;
+
+    let main = document.querySelector('main');
+    main.style.color = wybrany;}
