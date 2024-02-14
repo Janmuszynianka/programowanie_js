dd6e9c0958b13204e1866bdc44ec1124f14f23cb
Marcel <40071392+Janmuszynianka@users.noreply.github.com>
2024-02-14 14:54:00 +0100

    14.02.2024

---
+++ mr. Sosnowski/14.02.2024/GALERIA_MIEJSC/script.js
@@ -0,0 +1,50 @@
+var licznik=0;
+function podmien()
+{
+    licznik++;
+    if (licznik%2!=0)
+    {
+        document.getElementById("ikona").src="icon-on.png";
+        console.log(licznik);
+    }
+    else
+    {
+        document.getElementById("ikona").src="icon-off.png";
+        console.log(licznik);
+    }
+}
+
+function zmienZdjecie(zdjecie)
+{
+    if(zdjecie=="lanzarotte")
+    {
+        document.getElementById("zdjecieglowne").src="lanzarotte.jpg";
+    }
+    else if(zdjecie=="pekin")
+    {
+        document.getElementById("zdjecieglowne").src="pekin.jpg";
+    }
+    else if(zdjecie=="serengeti")
+    {
+        document.getElementById("zdjecieglowne").src="serengeti.jpg";
+    }
+    else if(zdjecie=="tajlandia")
+    {
+        document.getElementById("zdjecieglowne").src="tajlandia.jpg";
+    }
+    else if(zdjecie=="wenecja")
+    {
+        document.getElementById("zdjecieglowne").src="wenecja.jpg";
+    }
+
+function zmiana(){
+    var ikona = document.getElementById("icon");
+    if(ikona.src.match("icon-off.png"))
+    {
+        ikona.src="icon-on.png";
+    }
+    else
+    {
+        ikona.src="icon-off.png";
+    }
+}
